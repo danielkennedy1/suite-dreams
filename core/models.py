6c47c126f5f9f03302d504bba476681deac36da7
danielkennedy1 <danielrkennedy03@gmail.com>
2023-04-20 15:54:29 +0100

    Room and Booking models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+class Booking(models.Model):
+    id = models.IntegerField(primary_key=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    date = models.DateField()
+
+    def __str__(self):
+        return self.room.name
