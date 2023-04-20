f36291cab7b5c76fdc51c2de64665110995eba1a
Adam Byrne <adamrbyrne@gmail.com>
2023-04-20 16:48:06 +0100

    skeleton but good

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("book/", views.book, name="book"),
+    path("", views.index, name="index"),
 ]
--- core/views.py
+++ core/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html')
+def book(request):
+    return render(request, 'book.html')
+
+def index(request):
+    return render(request, 'index.html')
