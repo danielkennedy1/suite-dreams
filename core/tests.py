708163b9ef9fae3b680bfb953752e3957a3b92ac
danielkennedy1 <danielrkennedy03@gmail.com>
2023-04-23 15:33:29 +0100

    Test room-booking association

--- core/tests.py
+++ core/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from core.factories import RoomFactory
+from core.factories import BookingFactory
 
 class RoomTestCase(TestCase):
     def test_room_creation(self):
@@ -7,5 +8,11 @@ class RoomTestCase(TestCase):
         self.assertEqual(room.name, "Test Room")
         self.assertEqual(room.capacity, 10)
         
+class BookingTestCase(TestCase):
+    def setUp(self):
+        self.room = RoomFactory()
+        self.booking = BookingFactory.build(room=self.room)
 
-		
+    def test_booking_room_association(self):
+        self.assertEqual(self.booking.room, self.room)
+        
