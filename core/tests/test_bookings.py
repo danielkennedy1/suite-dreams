3e7d32dd46039cad942e1a08bf728025b606f252
Adam Byrne <adamrbyrne@gmail.com>
2023-04-25 13:48:57 +0100

    past booking

--- core/tests/test_bookings.py
+++ core/tests/test_bookings.py
@@ -1,7 +1,5 @@
 from django.test import TestCase
 
-
-
 class DoubleBooking(TestCase):
     def setUp(self) -> None:
         self.client.post('/book', data={
@@ -24,4 +22,22 @@ class DoubleBooking(TestCase):
             'end_time': '10:00',  
 		})
         self.assertEqual(res.status_code, 301)
+        
+class PastBooking(TestCase):       
+    def test_past_booking(self):
+        res = self.client.post('/book', data={
+                      'organiser': 'John',
+                      'title': 'Meeting',
+                        'title': 'Meeting',
+                        'details': 'Discuss the project',
+                        'date': '2001-01-01',
+                        'start_time': '09:00',
+                        'end_time': '10:00',
+                        'room': 3
+                        })
+        
+        self.assertEqual(res.status_code, 301)
+            
+                
+
       
