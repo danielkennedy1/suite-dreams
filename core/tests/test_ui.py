d39aa0b5fa49134438edb14346bbc4f0d3e10104
Adam Byrne <adamrbyrne@gmail.com>
2023-04-28 10:43:42 +0100

    basic e2e working

--- core/tests/test_ui.py
+++ core/tests/test_ui.py
@@ -1,9 +1,21 @@
-from django.test import TestCase
+# django test case that runs selenium tests
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-# No bookings => empty table with message "No bookings"
-class EmptyTable(TestCase):
-	def setUp(self):
-		self.client.get('/', data=None)
 
-	def test_empty_table(self):
-		self.assertContains(self.client.get('/'), 'No bookings')
+class NewVisitorTest(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome(headless=True)
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get(self.live_server_url)
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('Book | Suite Dreams', self.browser.title)
